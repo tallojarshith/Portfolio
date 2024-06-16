58a9384029e6532f1146e45cc0fc4870eb7f4bff
yuvakishore <yuvakishore143@gmail.com>
2024-06-16 14:20:51 +0530

    lading Page Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import "./styles/main.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="appContainer ">
+      <Navbar />
+      <div className="appInnerContainer">
+        <Landing />
+        <About />
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='aboutContainer d-flex  justify-content-center'>
+            <div className='innerContainer row'>
+                <div className='aboutInfoInnerCont col-sm-12 col-md-6'>
+                    <h2>About</h2>
+                    <p>Hii there! Myself Rohan and As an MLOps specialist,Expert ML/DL Engineer and MLOps Specialist: Leveraging Advanced Technologies for Optimal Results. With a deep passion for Machine Learning (ML), Deep Learning (DL), and MLOps, I am an accomplished engineer skilled in harnessing the power of modern web technologies to build exceptional websites. My expertise extends beyond aesthetics and functionality; I have a comprehensive understanding of ML and DL algorithms, frameworks, and techniques that enable me to create intelligent systems capable of analyzing vast amounts of data and delivering accurate predictions.</p>
+                    <hr />
+                    <p>Hii there! Myself Rohan and As an MLOps specialist,Expert ML/DL Engineer and MLOps Specialist: Leveraging Advanced Technologies for Optimal Results. With a deep passion for Machine Learning (ML), Deep Learning (DL), and MLOps, I am an accomplished engineer skilled in harnessing the power of modern web technologies to build exceptional websites. My expertise extends beyond aesthetics and functionality; I have a comprehensive understanding of ML and DL algorithms, frameworks, and techniques that enable me to create intelligent systems capable of analyzing vast amounts of data and delivering accurate predictions.</p>
+                </div>
+                <div className='aboutSkillsInnerCont col-sm-12 col-md-6'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
